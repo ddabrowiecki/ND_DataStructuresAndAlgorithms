2b099d58c78a7e5600f4b54c7886383230db57d0
Dominic Dabrowiecki <dominic.dabrowiecki@reddit.com>
2022-10-04 08:19:53 +0000

    Solve file recursion problem

--- show_me_the_data_structures/problem_2_file_recursion.py
+++ show_me_the_data_structures/problem_2_file_recursion.py
@@ -1,31 +1,7 @@
 import os
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.children = []
 
-    def get_value(self):
-        return self.value
-
-    def set_child(self, value):
-        self.children.append(value)
-
-    def get_children(self):
-        return self.children
-
-    def __repr__(self):
-        return f"Node({self.get_value()})"
-
-    def __str__(self):
-        return f"Node({self.get_value()})"
-
-class Tree:
-    def __init__(self, value):
-        self.root = Node(value)
-
-    def get_root(self):
-        return self.root
-
+# Got inspiration from this mentor help post, which resembles my original solution at the bottom:
+# https://knowledge.udacity.com/questions/447861
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
@@ -42,32 +18,42 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    file_visit_order = list()
-    folder_visit_order = list()
-    #recursive function
-    def traverse(node):
-        if node.startswith('subsub'):
-            print(os.path.isdir(node))
+    files = list()
+    folders = list()
+    # Look through all nodes of the path. Separate files and folders.
+    for node in os.listdir(path):
         if node.endswith(suffix):
-            pass
-            # visit_order.append(node)
-        if os.path.isdir(node):
-            for child in os.listdir(node):
-                traverse(child)
-    traverse(path)
-    return visit_order
+            files.append(node)
+        if "." not in node:
+            folders.append(node)
+    for folder in folders:
+        files.extend(find_files(suffix, path + "/" + folder ))
+    return files
 
 
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
 
-path = './testdir'
-os.chdir(path)
-cwd = os.getcwd()
+path = os.getcwd() + '/testdir'
 
 # Test Case 1
-print(find_files('.c', cwd))
+print(find_files('.c', path))
 
 # Test Case 2
 
 # Test Case 3
+
+
+# Original attempt at recursive solution
+
+    # visit_order = list()
+    # def traverse(node):
+    #     if node.startswith('subsub'):
+    #         print(os.path.isdir(node))
+    #     if node.endswith(suffix):
+    #         visit_order.append(node)
+    #     if os.path.isdir(node):
+    #         for child in os.listdir(node):
+    #             traverse(child)
+    # traverse(path)
+    # return visit_order
